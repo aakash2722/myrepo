0124edf1d5e5417ccfdcc72688917cc9d1029204
aakash2722 <61941113+aakash2722@users.noreply.github.com>
2020-08-15 14:06:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+var os=String.empty;
+
 function btnclick(){
     var a=document.getElementById("txtep").value;
     var b=document.getElementById("txtv1s").value;
 
     var res=a / (0.01 * b);
     document.getElementById("txtnos").value=res;
+    document.getElementById("qs").value = os;
 
 }
 
 function getquerystring(){
-const params = new URLSearchParams(window.location.search)
+  const params = new URLSearchParams(window.location.search)
 for (const param of params) {
-  console.log(param)
+  console.log(param);
+  os=param[1];
+  
 }
+console.log(os);
+
 }
+
+window.onload = getquerystring();
+
+
+
